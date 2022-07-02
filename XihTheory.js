db51d3d6aacb13fa0d27dcd8208f233698ffd7bd
TheCreator2022 <106817598+TheCreator2022@users.noreply.github.com>
2022-07-02 16:26:29 -0300

    Rename Theory
    Grass Clicker -> Xih Theory

--- XihTheory.js
+++ XihTheory.js
@@ -5,7 +5,7 @@ import { theory } from "../api/Theory";
 import { Utils } from "../api/Utils";
 
 var id = "xih";
-var name = "Grass Clicker";
+var name = "Xih Theory";
 var description = "wowowo";
 var authors = "The Creator 2022";
 var version = 1;
