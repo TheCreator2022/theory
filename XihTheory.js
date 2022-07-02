e45324ffe4c4329df1241ae9cded40041e34f1a0
TheCreator2022 <106817598+TheCreator2022@users.noreply.github.com>
2022-07-02 16:21:16 -0300

    Add files via upload

---
+++ XihTheory.js
@@ -0,0 +1,19 @@
+import { ExponentialCost, FirstFreeCost, LinearCost } from "../api/Costs";
+import { Localization } from "../api/Localization";
+import { parseBigNumber, BigNumber } from "../api/BigNumber";
+import { theory } from "../api/Theory";
+import { Utils } from "../api/Utils";
+
+var id = "xih";
+var name = "Grass Clicker";
+var description = "wowowo";
+var authors = "The Creator 2022";
+var version = 1;
+
+var currency;
+
+var init = () => {
+    currency = theory.createCurrency();
+}
+
+init();
