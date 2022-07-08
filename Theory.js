f366b0bedb1e69a42d87259bc34d44eab82e4063
TheCreator2022 <106817598+TheCreator2022@users.noreply.github.com>
2022-07-08 16:01:41 -0300

    Update Theory.js

--- Theory.js
+++ Theory.js
@@ -14,12 +14,34 @@ var currency;
 
 var init = () => {
   currency = theory.createCurrency();
+  
+   // a1
+    {
+        let getDesc = (level) => "a_1=" + getA1(level).toString(0);
+        a1 = theory.createUpgrade(0, currency, new FirstFreeCost(new ExponentialCost(15, Math.log2(2))));
+        a1.getDescription = (_) => Utils.getMath(getDesc(a1.level));
+        a1.getInfo = (amount) => Utils.getMathTo(getDesc(a1.level), getDesc(a1.level + amount));
+    }
+}
+
+var tick = (elapsedTime, multiplier) => {
+    let dt = BigNumber.from(elapsedTime * multiplier);
+    let bonus = theory.publicationMultiplier;
+    currency.value += dt * bonus * getC1(c1.level).pow(getC1Exponent(c1Exp.level)) *
+                                   getC2(c2.level).pow(getC2Exponent(c2Exp.level));
 }
 
 var getPrimaryEquation = () => {
-    let result = "\\dot{\\rho} = 0";
+    let result = "\\dot{\\rho} = a_1";
 
     return result;
 }
+
+var getPublicationMultiplier = (tau) => tau.pow(0.1) / BigNumber.THREE;
+var getPublicationMultiplierFormula = (symbol) => "\\frac{{" + symbol + "}^{0.1}}{3}";
+var getTau = () => currency.value;
+var get2DGraphValue = () => currency.value.sign * (BigNumber.ONE + currency.value.abs()).log10().toNumber();
+
+var getA1 = (level) => Utils.getStepwisePowerSum(level, 2, 10, 0);
 
 init();
